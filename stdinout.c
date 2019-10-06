5ba701889172ef640bf6ed38a9bba2d94de75f78
G <gvnwltrs@protonmail.com>
2019-10-05 22:17:48 -0400

    Add files via upload

---
+++ stdinout.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main() 
+{
+	int n; 
+	printf("Hello there! Go ahead and enter a number please: ");
+	scanf("%d", &n); //read input from STDIN
+	printf("Your number is: %d\n", n); //print ouput integer to STDOUT
+	return 0; 
+}
